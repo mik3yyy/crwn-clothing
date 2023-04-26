1328dc46204af10a9a97feb7919c788733e780dd
mik3yyy <mikeokpechi@gmail.com>
2023-04-26 10:00:56 +0100

    upadting directory page

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,21 @@
+import CategoryItem from "../category-item/category-item.component.jsx"
+import  './directory.styles.scss'
+
+ function Directory ({categories}){
+
+    
+    return (
+    <div className="categories-container">
+      
+      {
+      categories.map((category) => (
+        <CategoryItem key={category.id} category={category}/>
+      ))
+      }
+     
+      
+    </div>
+    )
+}
+
+ export default Directory;
